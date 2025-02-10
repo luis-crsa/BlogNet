352e71d0ef7812626745554063abfda2d2aac997
Luis <luiscrsarmento@gmail.com>
2025-02-10 08:50:43 -0300

    Blogs preview

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 const Home = () => {
+
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'Maria Fernanda', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Paulo Henrique', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Luís Cláudio', id: 3 }
+      ])
+
     return (
         <div className="home">
-            <h2>Homepage</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
         </div>
     );
 }
