1f05a01ae354d6a6b80ccfa12dc94a423ccb12bc
Luis <luiscrsarmento@gmail.com>
2025-02-13 14:48:44 -0300

    Fetch data from JSON server

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'Maria Fernanda', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Paulo Henrique', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Luís Cláudio', id: 3 }
-      ])
+    const [blogs, setBlogs] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs').then(res => {
+            if (!res.ok) {
+               throw Error('Could not fetch the data for that resource') 
+            }
+            return res.json();
+        }).then(data => {
+            setIsLoading(false);
+            setBlogs(data);
+            setError(null);
+        }).catch(err => {
+            setIsLoading(false);
+            setError(err.message);
+        })
+    }, []);
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title={"All Blogs"}/>
+            {error && <div>{error}</div>}
+            {isLoading && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title={"All Blogs"} />}
         </div>
     );
 }
