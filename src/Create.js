4298edeaefd556171bd2c45fcd7e1c020210fcf2
Luis <luiscrsarmento@gmail.com>
2025-02-20 20:07:19 -0300

    Add new blog

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,57 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Luís Cláudio');
+    const [isLoading, setIsLoading] = useState(false);
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = {title, body, author};
+
+        setIsLoading(true)
+
+        fetch('http://localhost:8000/blogs/', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log('New blog added');
+            setIsLoading(false)
+            history.push('/')
+        })
+    }
+
     return (
         <div className="create">
-            <h2>Add a New Blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog title: </label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Blog body: </label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}>
+                </textarea>
+                <label>Blog author: </label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="Luís Cláudio">Luís Cláudio</option>
+                    <option value="Maria Fernanda">Maria Fernanda</option>
+                </select>
+                {!isLoading && <button>Add blog</button>}
+                {isLoading && <button disabled>Adding blog...</button>}
+            </form>
         </div>
     );
 }
--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import useFetch from "./useFetch";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const {data: blogs, isLoading, error} = useFetch("http://localhost:8000/blogs");
+    const {data: blogs, isLoading, error} = useFetch('http://localhost:8000/blogs');
 
     return (
         <div className="home">
