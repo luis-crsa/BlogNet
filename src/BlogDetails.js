f7a2e548a0dcfe1890a426153e192df20449e831
Luis <luiscrsarmento@gmail.com>
2025-02-21 10:36:16 -0300

    Adding and deleting simulation time

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,18 +1,23 @@
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import useFetch from "./useFetch";
-import { useHistory } from "react-router-dom";
+import { useState } from "react";
 
 const BlogDetails = () => {
     const {id} = useParams();
     const {data: blog, isLoading, error} = useFetch('http://localhost:8000/blogs/' + id);
+    const [isPending, setIsPending] = useState(false);
     const history = useHistory();
 
     const handleClick = () => {
-        fetch('http://localhost:8000/blogs/' + blog.id, {
-            method: 'DELETE'
-        }).then(() => {
-            history.push('/');
-        })
+        setIsPending(true);
+
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs/' + blog.id, {
+                method: 'DELETE'
+            }).then(() => {
+                history.push('/');
+            })
+        }, 1000)
     }
 
     return (
@@ -24,7 +29,8 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
                     <div>{blog.body}</div>
-                    <button onClick={handleClick}>Delete</button>
+                    {!isPending && <button onClick={handleClick}>Delete</button>}
+                    {isPending && <button disabled>Deleting...</button>}
                 </article>
             )}
         </div>
--- src/Create.js
+++ src/Create.js
@@ -5,24 +5,26 @@ const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('Luís Cláudio');
-    const [isLoading, setIsLoading] = useState(false);
+    const [isPending, setIsPending] = useState(false);
     const history = useHistory();
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const blog = {title, body, author};
 
-        setIsLoading(true)
+        setIsPending(true);
 
-        fetch('http://localhost:8000/blogs/', {
-            method: 'POST',
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(blog)
-        }).then(() => {
-            console.log('New blog added');
-            setIsLoading(false)
-            history.push('/')
-        })
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs/', {
+                method: 'POST',
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(blog)
+            }).then(() => {
+                console.log('New blog added');
+                setIsPending(false);
+                history.push('/');
+            })
+        }, 1000);
     }
 
     return (
@@ -49,8 +51,8 @@ const Create = () => {
                     <option value="Luís Cláudio">Luís Cláudio</option>
                     <option value="Maria Fernanda">Maria Fernanda</option>
                 </select>
-                {!isLoading && <button>Add blog</button>}
-                {isLoading && <button disabled>Adding blog...</button>}
+                {!isPending && <button>Add blog</button>}
+                {isPending && <button disabled>Adding blog...</button>}
             </form>
         </div>
     );
